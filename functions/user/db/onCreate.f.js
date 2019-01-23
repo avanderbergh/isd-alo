6fe1f966c746f738e16149c60b03d0d3e02f656f
Mike DeBorde <mike.deborde@gmail.com>
2019-01-23 17:00:46 +0100

    working with pass and creating claims

--- functions/user/db/onCreate.f.js
+++ functions/user/db/onCreate.f.js
@@ -1,6 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-try { admin.initializeApp(functions.config().firebase) } catch (e) { console.log(e) }
+
+var serviceAccount = require("../../serviceAccountKey.json");
+
+
+try { admin.initializeApp(
+        {credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://isdcoaching-dev.firebaseio.com"}) }
+ catch (e) { console.log(e) }
+
 // Get a database reference
 var db = admin.firestore();
 try { db.settings({timestampsInSnapshots:true}) } catch (e) { console.log(e) }
@@ -25,55 +33,121 @@ const cors = require('cors')({origin: true});
 
 const util = require('../../utility/pass.js');
 
-exports = module.exports = functions.auth.user().onCreate((user) => {
-    console.log("update from pass");
-    
-	var userObject = {
-		displayName : user.displayName,
-		email : user.email,
-		photoUrl : user.photoURL,
-		createdOn : user.metadata.createdAt
-    };
-    console.log(userObject);
-    
-        Promise.resolve()
-            .then(()=>{
-                return util.wcbs22_get_token(); // return the auth with token
-            })
-            .then((auth) => { 
-                
-                queryUrl = util.wcbs22_prepare_query({endpoint: "Pupils",
-                                         filter: "Name/EmailAddress eq '"+user.email+"' and Form/AcademicYear eq 2018",
-                                         select: "NameId"}); // prepare query
-
-                queryParams={ headers:{'Authorization':'Bearer ' +auth.access_token, 'User-Agent':'isd-sync-services'},
-                      method:"GET"};
         
-               
-                return util.queryAPI(queryUrl,queryParams,"pupil-Query");
-              
+
+
+exports = module.exports = functions.auth.user().onCreate((user) => {
+    return new Promise(
+        (resolve, reject) => {
+        
+            queryUrl = util.wcbs22_prepare_token_query();
+
+            const params = new URLSearchParams();
+                    //var params = new URLSearchParams();
+            params.append('grant_type','client_credentials');
+            params.append('client_id',functions.config().wcbs22.id);
+            params.append('client_secret',functions.config().wcbs22.secret);
+
+            queryParams={
+                headers:{'User-Agent':'isd-sync-services'},
+                body:params,
+                method:"POST"
+                };
+
+            util.queryAPI(queryUrl,queryParams,"token")
+
+            .then((auth)=>{
+                
+                queryParams={ headers:{'Authorization':'Bearer ' +auth.access_token, 
+                            'User-Agent':'isd-sync-services'},
+                            method:"GET"};
+
+                studentQueryUrl = util.wcbs22_prepare_query({
+                    endpoint: "Pupils",
+                    filter: "Name/EmailAddress eq '"+user.email+"' and Form/AcademicYear eq 2018",
+                    select: "NameId"}); // prepare query
+
+                staffQueryUrl = util.wcbs22_prepare_query({
+                    endpoint: "Staff",
+                    expand: "Name($select = Id)",
+                    filter: "InternalEmailAddress  eq '"+user.email+"'",
+                    select: "Code"}); // prepare query
+
+                        
+                const student = util.queryAPI(studentQueryUrl,queryParams,"pupil-Query");
+                
+                const staff = util.queryAPI(staffQueryUrl,queryParams,"staff-Query");
+        
+                return Promise.all([student,staff])
+
+            })
+            .then((results) => {
+
+                if (results[0].value.length >=1)
+                    { userType = {type : 'student', id: results[0].value[0].NameId} }
+            
+                else if (results[1].value.length >=1)
+                    { userType = {type : 'staff', id: results[1].value[0].Name.Id} }
+                
+                return userType
+            })
+            .then((userType) =>{
+                // process userType
+                console.log("Existing Roles");
+                return getClaim(user.uid)
             })
             .then((res) =>{
-                pupilResult = res.value; // save result
+                console.log(userType);
 
-                queryUrl = util.wcbs22_prepare_query({endpoint: "Staff",
-                filter: "InternalEmailAddress  eq '"+user.email+"'",
-                select: "Id"}); // prepare query
-
-                return util.queryAPI(queryUrl,queryParams,"staff-Query");
-               
-                // return the query data
+                //console.log(user.uid);
+            
+                const customClaims = { [userType.type]: true };
+                // Set custom user claims on this newly created user.
+                setClaim(user, customClaims)
+                resolve(userType);
+                return
             })
-            .then((res) => {
-                staffResult = res.value;// save result
-                userType = staffResult.length === 0 ? "student" : "staff";
-                return console.log(userType); 
-            })
-            .catch(error=>console.log(error))
-    //})
-       
-    return userObject;
-  })
 
- 
+            .catch(error=>console.log(error));
 
+      
+        
+    })
+
+})
+
+function getClaim(uid){
+    return new Promise(
+        (resolve, reject) => {
+        
+    admin.auth().getUser(uid).then((userRecord) => {
+        // The claims can be accessed on the user record.
+        try{
+            resolve(console.log(userRecord.customClaims.staff));
+        }
+        catch(err){
+            resolve(console.log("No Roles Defined"));
+        }
+        return
+
+      })
+      .catch(error => {
+        console.log(error);
+        });
+})
+}
+
+function setClaim(user, customClaims){
+
+    return admin.auth().setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+    // Update real-time database to notify client to force refresh.
+    const metadataRef = admin.database().ref("metadata/" + user.uid);
+    // Set the refresh time to the current UTC timestamp.
+    // This will be captured on the client to force a token refresh.
+    return metadataRef.set({refreshTime: new Date().getTime()});
+    })
+    .catch(error => {
+    console.log(error);
+    });
+}
--- functions/utility/pass.js
+++ functions/utility/pass.js
@@ -1,11 +1,16 @@
 const functions = require('firebase-functions');
 const endPointUrl = 'https://3sys.isdedu.de/Wcbs.API.2.2/api/';
+
 var fetch = require("fetch-retry");
 require('url-search-params-polyfill');
 
 
 module.exports =  {
+    /***************************************************************
+     * retrieves the authentication token 
+     ***************************************************************/
     wcbs22_get_token: function(){
+
         queryUrl = endPointUrl+'token';
         const params = new URLSearchParams();
                 //var params = new URLSearchParams();
@@ -19,8 +24,19 @@ module.exports =  {
             method:"POST"
             };
 
-        return this.queryAPI(queryUrl,queryParams,"token");
+        return this.queryAPI(queryUrl,queryParams,"token") // this is a promise
+        
 
+    },
+
+    /***************************************************************
+     * prepare th query authentication token 
+     ***************************************************************/
+
+
+    wcbs22_prepare_token_query: function(){
+    
+        return endPointUrl+'token';
     },
 
     wcbs22_prepare_query: function(params){
@@ -34,34 +50,33 @@ module.exports =  {
     
         return endPointUrl+'schools(1)/'+params.endpoint+'?'+query;
   },
-  /* ***********************************
-Function to query an API
-util.queryAPI(endPointUrl+'token',otherParams,"token");
-************************************/
+    /* ***********************************
+    Function to query an API
+    util.queryAPI(endPointUrl+'token',otherParams,"token");
+    ************************************/
     queryAPI: function(url,params,desc){ 
         
-
-    return new Promise((resolve) => fetch(url,params,{
-      retries: 3,
-      retryDelay: 1000
-    })
-    // eslint-disable-next-line consistent-return
-    .then((res) => {
-      console.log(desc+": HEADERS "+ res.headers.get("content-type")); 
-      // eslint-disable-next-line promise/always-return
-      if (res.headers.get("content-type") !== null){
-          if (res.headers.get("content-type").includes("application/json")){
-            output = res.json();
-          }
-          else if (res.headers.get("content-type") === "image/jpeg") {
-            output = res.buffer(); // binary data in full
-          }
-      else {
-        console.log (res);
-      }
-      //console.log(output);
-      return output;
-    }})
-    .then((result) => resolve(result)));
+        return new Promise((resolve) => fetch(url,params,{
+            retries: 3,
+            retryDelay: 1000
+            })
+        // eslint-disable-next-line consistent-return
+        .then((res) => {
+        //console.log(desc+": HEADERS "+ res.headers.get("content-type")); 
+        // eslint-disable-next-line promise/always-return
+        if (res.headers.get("content-type") !== null){
+            if (res.headers.get("content-type").includes("application/json")){
+                output = res.json();
+            }
+            else if (res.headers.get("content-type") === "image/jpeg") {
+                output = res.buffer(); // binary data in full
+            }
+        else {
+            //console.log (res);
+        }
+        //console.log(output);
+        return output;
+        }})
+        .then((result) => resolve(result)));
   }
 }
